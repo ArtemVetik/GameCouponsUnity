1a28ffad0e93516f6f4395c88b14aba65331af0b
Artem Vetik <artem.vetik58@gmail.com>
2023-12-02 11:00:43 +0300

    Implemented basic methods from the API

---
+++ Packages/com.agava.gamecoupons/Runtime/DTO/RefreshResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine.Scripting;
+
+namespace Agava.GameCoupons
+{
+    [Serializable]
+    public struct RefreshResponse
+    {
+        [field: Preserve]
+        public string access;
+    }
+}
