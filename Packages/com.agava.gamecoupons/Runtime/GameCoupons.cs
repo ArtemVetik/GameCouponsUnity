894f4b7360c7277ce9f35c3f86d22e7961080c21
Artem Vetik <av.unitygames@gmail.com>
2023-12-02 12:20:01 +0300

    Fix authorizations and Refresh request

--- Packages/com.agava.gamecoupons/Runtime/GameCoupons.cs
+++ Packages/com.agava.gamecoupons/Runtime/GameCoupons.cs
@@ -9,23 +9,24 @@ namespace Agava.GameCoupons
     {
         private const string BaseAddress = "https://stage.game-coupon.com";
 
-        private static string _refreshToken;
+        private static LoginResponse _loginData;
 
-        public static async Task Login(int id, string password, Action<string> onErrorCallback = null)
+        public static async Task<bool> Login(int id, string password, Action<string> onErrorCallback = null)
         {
             var postData = JsonUtility.ToJson(new LoginRequest() { id = id, password = password });
             using var request = UnityWebRequest.Post($"{BaseAddress}/api/v1/login", postData, "application/json");
 
             await request.SendWebRequest();
 
-            var login = Parse<LoginResponse>(request, onErrorCallback);
-            _refreshToken = login.refresh;
+            _loginData = Parse<LoginResponse>(request, onErrorCallback);
+
+            return _loginData.access != null;
         }
 
         public static async Task<GenresResponse> Genres(int page = 1, int size = 50, Action<string> onErrorCallback = null)
         {
             using var request = UnityWebRequest.Get($"{BaseAddress}/api/v1/genres?page={page}&size={size}");
-            request.SetRequestHeader("Authorization", $"Bearer {_refreshToken}");
+            request.SetRequestHeader("Authorization", $"Bearer {_loginData.access}");
 
             await request.SendWebRequest();
 
@@ -35,7 +36,7 @@ namespace Agava.GameCoupons
         public static async Task<OrganizationsResponse> Organizations(int page = 1, int size = 50, Action<string> onErrorCallback = null)
         {
             using var request = UnityWebRequest.Get($"{BaseAddress}/api/v1/organizations?page={page}&size={size}");
-            request.SetRequestHeader("Authorization", $"Bearer {_refreshToken}");
+            request.SetRequestHeader("Authorization", $"Bearer {_loginData.access}");
 
             await request.SendWebRequest();
 
@@ -45,28 +46,34 @@ namespace Agava.GameCoupons
         public static async Task<PlatformsResponse> Platforms(int page = 1, int size = 50, Action<string> onErrorCallback = null)
         {
             using var request = UnityWebRequest.Get($"{BaseAddress}/api/v1/platforms?page={page}&size={size}");
-            request.SetRequestHeader("Authorization", $"Bearer {_refreshToken}");
+            request.SetRequestHeader("Authorization", $"Bearer {_loginData.access}");
 
             await request.SendWebRequest();
 
             return Parse<PlatformsResponse>(request, onErrorCallback);
         }
 
-        public static async Task<RefreshResponse> Refresh(Action<string> onErrorCallback = null)
+        public static async Task<bool> Refresh(Action<string> onErrorCallback = null)
         {
-            var postData = $"{{\"refresh\": {_refreshToken}}}";
+            var postData = $"{{\"refresh\": \"{_loginData.refresh}\"}}";
             using var request = UnityWebRequest.Post($"{BaseAddress}/api/v1/refresh", postData, "application/json");
 
             await request.SendWebRequest();
 
-            return Parse<RefreshResponse>(request, onErrorCallback);
+            var response = Parse<RefreshResponse>(request, onErrorCallback);
+
+            if (response.access == null)
+                return false;
+
+            _loginData.access = response.access;
+            return true;
         }
 
         private static T Parse<T>(UnityWebRequest request, Action<string> onErrorCallback) where T : struct
         {
             if (request.result != UnityWebRequest.Result.Success)
             {
-                onErrorCallback?.Invoke(request.error);
+                onErrorCallback?.Invoke($"{request.error}\n{request.downloadHandler.text}");
                 return default;
             }
 
